74d8e014f5d4439a7cabb9ff40b2b2d1586b3494
Yūri Katsuki <yuri@katsuki.moe>
2023-01-16 23:33:41 +0500

    added logs

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -8,12 +8,15 @@ export default async function handler(
 ) {
   // Initialize a bot instance
   const bot = new Bot(process.env.TOKEN || "");
+  console.log("Initialized instance successfully")
 
   // Append all middlewares to bot
   await functions(bot);
+  console.log("Appended functions successfully")
 
   // Create a request handler
   const handle = webhookCallback(bot, "next-js");
+  console.log("Created handler successfully")
 
   // Return the handler to Vercel api
   return await handle(req, res);
