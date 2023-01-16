da230c1de09fa9cbb08cdddb01496c4d4af2ba90
Yūri Katsuki <yuri@katsuki.moe>
2023-01-16 23:06:19 +0500

    uploading formatted version

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,12 +1,15 @@
 import { Bot, webhookCallback } from "grammy";
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const token = process.env.TOKEN || ""
-const bot = new Bot(token);
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const token = process.env.TOKEN || "";
+
+  if (token) throw new Error("No Telegram bot token!");
+
+  const bot = new Bot(token);
+
   await webhookCallback(bot, "http");
 }
