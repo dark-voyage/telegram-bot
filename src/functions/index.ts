306c7776ce8497db560736ee3cd51f3818635630
Yūri Katsuki <yuri@katsuki.moe>
2023-04-20 11:17:33 +0500

    updating yurio's telegram homepage

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,14 +1,10 @@
 import { Bot } from "grammy";
 import help from "@/functions/help";
 import start from "@/functions/start";
-import minecraft from "@/functions/minecraft";
-import gitlab from "@/functions/gitlab";
 
 const functions = async (bot: Bot) => {
   await bot.use(help);
   await bot.use(start);
-  await bot.use(gitlab);
-  await bot.use(minecraft);
 };
 
 export default functions;
